252624ffd6481a69bbca97291ea01c168b1c7263
tonmoyt <83818796+tonmoyt@users.noreply.github.com>
2023-10-27 06:17:57 -0700

    Add files via upload

---
+++ api/apimodel.go
@@ -0,0 +1,96 @@
+package api
+
+import (
+	"encoding/json"
+	"regexp"
+
+	"github.com/xtls/xray-core/infra/conf"
+)
+
+// Config API config
+type Config struct {
+	APIHost             string  `mapstructure:"ApiHost"`
+	NodeID              int     `mapstructure:"NodeID"`
+	Key                 string  `mapstructure:"ApiKey"`
+	NodeType            string  `mapstructure:"NodeType"`
+	EnableVless         bool    `mapstructure:"EnableVless"`
+	EnableXTLS          bool    `mapstructure:"EnableXTLS"`
+	Timeout             int     `mapstructure:"Timeout"`
+	SpeedLimit          float64 `mapstructure:"SpeedLimit"`
+	DeviceLimit         int     `mapstructure:"DeviceLimit"`
+	RuleListPath        string  `mapstructure:"RuleListPath"`
+	DisableCustomConfig bool    `mapstructure:"DisableCustomConfig"`
+}
+
+// NodeStatus Node status
+type NodeStatus struct {
+	CPU    float64
+	Mem    float64
+	Disk   float64
+	Uptime uint64
+}
+
+type NodeInfo struct {
+	NodeType          string // Must be V2ray, Trojan, and Shadowsocks
+	NodeID            int
+	Port              uint32
+	SpeedLimit        uint64 // Bps
+	AlterID           uint16
+	TransportProtocol string
+	FakeType          string
+	Host              string
+	Path              string
+	EnableTLS         bool
+	TLSType           string
+	EnableVless       bool
+	CypherMethod      string
+	ServerKey         string
+	ServiceName       string
+	Header            json.RawMessage
+	NameServerConfig  []*conf.NameServerConfig
+}
+
+type UserInfo struct {
+	UID           int
+	Email         string
+	Passwd        string
+	Port          uint32
+	Method        string
+	SpeedLimit    uint64 // Bps
+	DeviceLimit   int
+	Protocol      string
+	ProtocolParam string
+	Obfs          string
+	ObfsParam     string
+	UUID          string
+	AlterID       uint16
+}
+
+type OnlineUser struct {
+	UID int
+	IP  string
+}
+
+type UserTraffic struct {
+	UID      int
+	Email    string
+	Upload   int64
+	Download int64
+}
+
+type ClientInfo struct {
+	APIHost  string
+	NodeID   int
+	Key      string
+	NodeType string
+}
+
+type DetectRule struct {
+	ID      int
+	Pattern *regexp.Regexp
+}
+
+type DetectResult struct {
+	UID    int
+	RuleID int
+}
---
+++ service/controller/config.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"github.com/XrayR-project/XrayR/common/limiter"
+	"github.com/XrayR-project/XrayR/common/mylego"
+)
+
+type Config struct {
+	ListenIP                string                           `mapstructure:"ListenIP"`
+	SendIP                  string                           `mapstructure:"SendIP"`
+	UpdatePeriodic          int                              `mapstructure:"UpdatePeriodic"`
+	CertConfig              *mylego.CertConfig               `mapstructure:"CertConfig"`
+	EnableDNS               bool                             `mapstructure:"EnableDNS"`
+	DNSType                 string                           `mapstructure:"DNSType"`
+	DisableUploadTraffic    bool                             `mapstructure:"DisableUploadTraffic"`
+	DisableGetRule          bool                             `mapstructure:"DisableGetRule"`
+	EnableProxyProtocol     bool                             `mapstructure:"EnableProxyProtocol"`
+	EnableFallback          bool                             `mapstructure:"EnableFallback"`
+	DisableIVCheck          bool                             `mapstructure:"DisableIVCheck"`
+	DisableSniffing         bool                             `mapstructure:"DisableSniffing"`
+	AutoSpeedLimitConfig    *AutoSpeedLimitConfig            `mapstructure:"AutoSpeedLimitConfig"`
+	GlobalDeviceLimitConfig *limiter.GlobalDeviceLimitConfig `mapstructure:"GlobalDeviceLimitConfig"`
+	FallBackConfigs         []*FallBackConfig                `mapstructure:"FallBackConfigs"`
+}
+
+type AutoSpeedLimitConfig struct {
+	Limit         int `mapstructure:"Limit"` // mbps
+	WarnTimes     int `mapstructure:"WarnTimes"`
+	LimitSpeed    int `mapstructure:"LimitSpeed"`    // mbps
+	LimitDuration int `mapstructure:"LimitDuration"` // minute
+}
+
+type FallBackConfig struct {
+	SNI              string `mapstructure:"SNI"`
+	Alpn             string `mapstructure:"Alpn"`
+	Path             string `mapstructure:"Path"`
+	Dest             string `mapstructure:"Dest"`
+	ProxyProtocolVer uint64 `mapstructure:"ProxyProtocolVer"`
+}
